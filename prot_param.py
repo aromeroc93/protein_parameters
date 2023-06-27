73584f7e2ebcd3d0908c7876c1947776647f06e6
Àlex Romero-Casañas <aromero@immunostep.com>
2023-06-27 12:13:04 +0200

    Added Abs 0.1% calculation

--- prot_param.py
+++ prot_param.py
@@ -4,8 +4,9 @@ prot = input("Insert protein sequence: ")
 protein = ProteinAnalysis(prot)
 
 epsilon = protein.molar_extinction_coefficient()
+mw = protein.molecular_weight()
 
 print("Number of residues: ", sum(protein.count_amino_acids().values()))
-print("Mw: ""%0.2f" % protein.molecular_weight())
+print("Mw: ""%0.2f" % mw)
 print("IP: ""%0.2f" % protein.isoelectric_point())
-print("Extinction coefficient:", epsilon[0], "(reduced), ", epsilon[1], "(oxidized)")
+print("Extinction coefficient:", epsilon[0], "(reduced), ", epsilon[1], "(oxidized)", "\nAbs280nm (1g/l):", epsilon[0]/mw, " // ", 1/(epsilon[0]/mw))
